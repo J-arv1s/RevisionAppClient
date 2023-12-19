45ae37f87286bb310c92fcc3d9cf4208db540044
Billy <s.pbilly@yahoo.co.uk>
2023-12-19 17:13:23 +0000

    Teacher subjects and subjects page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
 export { default as RegisterPage } from "./RegisterPage"
+export {default as LoginPage} from './LoginPage'
+export {default as StudentPage} from './StudentPage'
+export {default as SubjectsPage} from './SubjectsPage'
