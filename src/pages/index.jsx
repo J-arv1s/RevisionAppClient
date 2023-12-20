760f339d9411dd5b562e1f25d17fe03f61604ab2
Billy <s.pbilly@yahoo.co.uk>
2023-12-20 09:40:18 +0000

    merging with staging

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,3 @@
-export { default as RegisterPage } from "./RegisterPage"
 export {default as LoginPage} from './LoginPage'
 export {default as StudentPage} from './StudentPage'
 export {default as SubjectsPage} from './SubjectsPage'
-export {default as QuizPage} from './QuizPage'
