e77b10c261ad22e2b9b4492671a0b2a0b63a6380
Billy <s.pbilly@yahoo.co.uk>
2023-12-20 16:44:10 +0000

    Subject Page functioning

--- src/components/TeacherSubjects/index.jsx
+++ src/components/TeacherSubjects/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {TeacherSubject} from '../index';
+import TeacherSubject from '../TeacherSubject'; 
 
-const TeacherSubjects = () => {
-  const subjects = ['Subject 1', 'Subject 2', 'Subject 3']; 
+const TeacherSubjects = ({ subjects, setSubjects }) => {
+  const handleDelete = (subjectName) => {
+    fetch(`http://localhost:3000/subjects/${subjectName}`, { method: 'DELETE' })
+      .then(() => {
+        console.log(`${subjectName} deleted`);
+        setSubjects(subjects.filter(subject => subject.subjectName !== subjectName));
+      })
+      .catch(error => console.error('Error deleting subject:', error));
+  };
 
   return (
     <>
-      {subjects.map((subject, index) => (
-        <TeacherSubject key={index} subjectName={subject} />
+      {subjects.map((subject) => (
+        <TeacherSubject 
+          key={subject._id} 
+          subject={subject} 
+          handleDelete={handleDelete} 
+        />
       ))}
     </>
   );
