bd242d34d30fa97e8c6743c13252644cdb0c9b99
smadeira22 <saramariaramosmadeira@gmail.com>
2023-12-20 17:44:59 +0000

    add pages

---
+++ src/AllPages/TeacherQuizesPage/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../index.css'
+
+const TeacherQuizesPage = () => {
+  return (
+    <>
+        <section id="teacher1">
+            <div className='first'>
+                <h2 id='h2Quiz'>Quizes</h2>
+                <div>
+                    <button id="addQuiz">+ Add a quiz</button>
+                </div>   
+            </div> 
+            <section id='quizes'>
+                <div className='buttons'>
+                    <p>Quiz 1</p>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+                <div className='buttons'>
+                    <p>Quiz 2</p>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+                <div className='buttons'>
+                    <p>Quiz 3</p>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            </section> 
+        </section>
+    </>
+  )
+}
+
+export default TeacherQuizesPage
