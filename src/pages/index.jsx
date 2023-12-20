c907968745e87855c6cdf3fee11340ee3681ac5d
Billy <s.pbilly@yahoo.co.uk>
2023-12-20 15:22:52 +0000

    functionality added to quiz page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,3 @@
 export {default as LoginPage} from './LoginPage'
 export {default as StudentPage} from './StudentPage'
 export {default as SubjectsPage} from './SubjectsPage'
-export {default as ProfilePage} from './ProfilePage'
