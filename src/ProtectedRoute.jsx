90f4414303ac61d76a5875737f55967d1f68edda
Billy <s.pbilly@yahoo.co.uk>
2023-12-21 09:48:43 +0000

    Route Protection

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { isLoggedIn } from './auth'; // adjust the import path as needed
+import { isLoggedIn } from './auth'; 
 
 const ProtectedRoute = ({ children }) => {
   if (!isLoggedIn()) {
-    // Redirect to the login page if not logged in
+    
     return <Navigate to="/" />;
   }
 
