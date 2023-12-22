d77acfa36ff5d85567e95994b267cf19a6034526
smadeira22 <saramariaramosmadeira@gmail.com>
2023-12-22 02:18:07 +0000

    edit teacher subject component

--- src/components/TeacherSubject/index.jsx
+++ src/components/TeacherSubject/index.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 
 const TeacherSubject = ({ subject, handleDelete }) => {
   return (
-    <div id='teach-info'>
+    <>
+    <section id='teach-info'>
       <h3 id='sub-name'>{subject.subjectName}</h3>
-      <button id='edit'>Edit</button>
-      <button id='delete' onClick={() => handleDelete(subject.subjectName)}>Delete</button>
-    </div>
+    
+        <button id='delete' onClick={() => handleDelete(subject.subjectName)}>Delete</button>
+   
+    </section>
+    </>
   );
 };
 
